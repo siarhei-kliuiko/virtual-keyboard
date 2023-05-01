11edb78de29ff335bc71713f3017008f7e3d51e8
siarhei-kliuiko <siarhei.kliuiko@gmail.com>
2023-05-01 08:01:05 +0300

    feat: add keyboard-layouts.js module with main keyboard layouts

---
+++ js-modules/keyboard-layouts.js
@@ -0,0 +1,519 @@
+export const keyCodes = ['Backquote', 'digit1', 'digit2', 'digit3', 'digit4', 'digit5', 'digit6', 'digit7', 'digit8', 'digit9', 'digit0', 'Minus', 'Equal', 'Backspace',
+  'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete',
+  'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter',
+  'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight',
+  'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
+
+export const keyMaps = {
+  special: {
+    backspace: {
+      default: 'Backspace',
+    },
+
+    tab: {
+      default: 'Tab',
+    },
+
+    delete: {
+      default: 'Del',
+    },
+
+    capslock: {
+      default: 'CapsLock',
+    },
+
+    enter: {
+      default: 'Enter',
+    },
+
+    shiftleft: {
+      default: 'Shift',
+    },
+
+    arrowup: {
+      default: '▲',
+    },
+
+    shiftright: {
+      default: 'Shift',
+    },
+
+    controlleft: {
+      default: 'Ctrl',
+    },
+
+    metaleft: {
+      default: 'Win',
+    },
+
+    altleft: {
+      default: 'Alt',
+    },
+
+    altright: {
+      default: 'Alt',
+    },
+
+    controlright: {
+      default: 'Ctrl',
+    },
+
+    arrowleft: {
+      default: '◄',
+    },
+
+    arrowdown: {
+      default: '▼',
+    },
+
+    arrowright: {
+      default: '►',
+    },
+  },
+
+  common: {
+    digit1: {
+      default: '1',
+      alt: '!',
+    },
+
+    digit5: {
+      default: '5',
+      alt: '%',
+    },
+
+    digit8: {
+      default: '8',
+      alt: '*',
+    },
+
+    digit9: {
+      default: '9',
+      alt: '(',
+    },
+
+    digit0: {
+      default: '0',
+      alt: ')',
+    },
+
+    minus: {
+      default: '-',
+      alt: '_',
+    },
+
+    equal: {
+      default: '=',
+      alt: '+',
+    },
+
+    space: {
+      default: ' ',
+      alt: ' ',
+    },
+  },
+
+  en: {
+    backquote: {
+      default: '`',
+      alt: '~',
+    },
+
+    digit2: {
+      default: '2',
+      alt: '@',
+    },
+
+    digit3: {
+      default: '3',
+      alt: '#',
+    },
+
+    digit4: {
+      default: '4',
+      alt: '$',
+    },
+
+    digit6: {
+      default: '6',
+      alt: '^',
+    },
+
+    digit7: {
+      default: '7',
+      alt: '&',
+    },
+
+    keyq: {
+      default: 'q',
+      upper: 'Q',
+    },
+
+    keyw: {
+      default: 'w',
+      upper: 'W',
+    },
+
+    keye: {
+      default: 'e',
+      upper: 'E',
+    },
+
+    keyr: {
+      default: 'r',
+      upper: 'R',
+    },
+
+    keyt: {
+      default: 't',
+      upper: 'T',
+    },
+
+    keyy: {
+      default: 'y',
+      upper: 'Y',
+    },
+
+    keyu: {
+      default: 'u',
+      upper: 'U',
+    },
+
+    keyi: {
+      default: 'i',
+      upper: 'I',
+    },
+
+    keyo: {
+      default: 'o',
+      upper: 'O',
+    },
+
+    keyp: {
+      default: 'p',
+      upper: 'P',
+    },
+
+    bracketleft: {
+      default: '[',
+      alt: '{',
+    },
+
+    bracketright: {
+      default: ']',
+      alt: '}',
+    },
+
+    backslash: {
+      default: '\\',
+      alt: '|',
+    },
+
+    keya: {
+      default: 'a',
+      upper: 'A',
+    },
+
+    keys: {
+      default: 's',
+      upper: 'S',
+    },
+
+    keyd: {
+      default: 'd',
+      upper: 'D',
+    },
+
+    keyf: {
+      default: 'f',
+      upper: 'F',
+    },
+
+    keyg: {
+      default: 'g',
+      upper: 'G',
+    },
+
+    keyh: {
+      default: 'h',
+      upper: 'H',
+    },
+
+    keyj: {
+      default: 'j',
+      upper: 'J',
+    },
+
+    keyk: {
+      default: 'k',
+      upper: 'K',
+    },
+
+    keyl: {
+      default: 'l',
+      upper: 'L',
+    },
+
+    semicolon: {
+      default: ';',
+      alt: ':',
+    },
+
+    quote: {
+      default: "'",
+      alt: '"',
+    },
+
+    keyz: {
+      default: 'z',
+      upper: 'Z',
+    },
+
+    keyx: {
+      default: 'x',
+      upper: 'X',
+    },
+
+    keyc: {
+      default: 'c',
+      upper: 'C',
+    },
+
+    keyv: {
+      default: 'v',
+      upper: 'V',
+    },
+
+    keyb: {
+      default: 'b',
+      upper: 'B',
+    },
+
+    keyn: {
+      default: 'n',
+      upper: 'N',
+    },
+
+    keym: {
+      default: 'm',
+      upper: 'M',
+    },
+
+    comma: {
+      default: ',',
+      alt: '<',
+    },
+
+    period: {
+      default: '.',
+      alt: '>',
+    },
+
+    slash: {
+      default: '/',
+      alt: '?',
+    },
+  },
+
+  ru: {
+    backquote: {
+      default: 'ё',
+      upper: 'Ё',
+    },
+
+    digit2: {
+      default: '2',
+      alt: '"',
+    },
+
+    digit3: {
+      default: '3',
+      alt: '№',
+    },
+
+    digit4: {
+      default: '4',
+      alt: ';',
+    },
+
+    digit6: {
+      default: '6',
+      alt: ':',
+    },
+
+    digit7: {
+      default: '7',
+      alt: '?',
+    },
+
+    keyq: {
+      default: 'й',
+      upper: 'Й',
+    },
+
+    keyw: {
+      default: 'ц',
+      upper: 'Ц',
+    },
+
+    keye: {
+      default: 'у',
+      upper: 'У',
+    },
+
+    keyr: {
+      default: 'к',
+      upper: 'К',
+    },
+
+    keyt: {
+      default: 'е',
+      upper: 'Е',
+    },
+
+    keyy: {
+      default: 'н',
+      upper: 'Н',
+    },
+
+    keyu: {
+      default: 'г',
+      upper: 'Г',
+    },
+
+    keyi: {
+      default: 'ш',
+      upper: 'Ш',
+    },
+
+    keyo: {
+      default: 'щ',
+      upper: 'Щ',
+    },
+
+    keyp: {
+      default: 'з',
+      upper: 'З',
+    },
+
+    bracketleft: {
+      default: 'х',
+      upper: 'Х',
+    },
+
+    bracketright: {
+      default: 'ъ',
+      upper: 'Ъ',
+    },
+
+    backslash: {
+      default: '\\',
+      alt: '/',
+    },
+
+    keya: {
+      default: 'ф',
+      upper: 'Ф',
+    },
+
+    keys: {
+      default: 'ы',
+      upper: 'Ы',
+    },
+
+    keyd: {
+      default: 'в',
+      upper: 'В',
+    },
+
+    keyf: {
+      default: 'а',
+      upper: 'А',
+    },
+
+    keyg: {
+      default: 'п',
+      upper: 'П',
+    },
+
+    keyh: {
+      default: 'р',
+      upper: 'Р',
+    },
+
+    keyj: {
+      default: 'о',
+      upper: 'О',
+    },
+
+    keyk: {
+      default: 'л',
+      upper: 'Л',
+    },
+
+    keyl: {
+      default: 'д',
+      upper: 'Д',
+    },
+
+    semicolon: {
+      default: 'ж',
+      upper: 'Ж',
+    },
+
+    quote: {
+      default: 'э',
+      upper: 'Э',
+    },
+
+    keyz: {
+      default: 'я',
+      upper: 'Я',
+    },
+
+    keyx: {
+      default: 'ч',
+      upper: 'Ч',
+    },
+
+    keyc: {
+      default: 'с',
+      upper: 'С',
+    },
+
+    keyv: {
+      default: 'м',
+      upper: 'М',
+    },
+
+    keyb: {
+      default: 'и',
+      upper: 'И',
+    },
+
+    keyn: {
+      default: 'т',
+      upper: 'Т',
+    },
+
+    keym: {
+      default: 'ь',
+      upper: 'Ь',
+    },
+
+    comma: {
+      default: 'б',
+      upper: 'Б',
+    },
+
+    period: {
+      default: 'ю',
+      upper: 'Ю',
+    },
+
+    slash: {
+      default: '.',
+      alt: ',',
+    },
+  },
+};
